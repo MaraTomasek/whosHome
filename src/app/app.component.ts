08a99f0c35dcf0e050e95f1a79e98c0064093535
Mara Tomasek <mara.tomasek@adesso.de>
2022-09-16 12:06:42 +0200

    added card and header components, deleted template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from "./model/user.model";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'whosHome';
+  users: User[];
+
+  constructor() {
+    let mara = new User("Mara");
+    let fani = new User("Fani");
+    let julia = new User("Julia");
+
+    this.users = [mara, fani, julia];
+  }
+
 }
